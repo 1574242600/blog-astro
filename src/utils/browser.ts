d3bbf2f0c7d81b35cf50e7ee02ccea7bc193a05f
Nworm <xiaosi1574@gmail.com>
2024-03-27 16:37:44 +0800

    ✨ (page): friends 页面新增 FriendsActivity 组件

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -20,3 +20,10 @@ export function addLoadEventListener(name: string, handler: () => void) {
 export function toDays(ms: number) {
     return Math.floor((ms / 1e3) / 86400)
 }
+
+export function dateToString(time: Date): string {
+    const y = time.getFullYear()
+    const m = time.getMonth() + 1
+    const d = time.getDate()
+    return `${y}-${m}-${d}`
+}
